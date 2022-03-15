de7cfe9209133d84afe1bc590927c5323bf47ae7
JesusA02 <cortesj02@estudiantec.cr>
2022-03-15 10:29:41 -0600

    Add Arreglo

--- Figura.java
+++ Figura.java
@@ -1,9 +1,10 @@
 public abstract class Figura{
     
     public abstract double calcularArea();
-  
 
     public abstract double calcularAreaC();
+
+    public abstract double calcularAreaT();
   
 
 }
